bc33b1ea07bf13fc09aa39fde18605c1aebf8757
Cláudio Roberto Celestino Júnior <112024522+ccclaudinho@users.noreply.github.com>
2023-07-02 10:13:44 -0300

    acrescenta funções e operadores básicos da Linguagem

--- programa1.py
+++ programa1.py
@@ -33,5 +33,57 @@ verifica="Vasco" in Times_cariocas
 
 print (verifica) #verdadeiro
 
-#Funções em Python >> usamos def para definir uma função
+#Funções em Python >> usamos def para definir uma função que é um bloco de código que é executado apenas quando é invocado Ex:
+# sintaxe: def NomeDaFunção(argumentos):
+
+def escreveBomdia(nome):
+   print ("Bom dia,", nome+"!")
+   
+escreveBomdia("Cláudio")
+
+def soma(num1,num2):
+    resultado =num1+num2;
+    print ("A soma entre os números", num1,"e", num2, "é", resultado)
+
+soma(8,9)
+
+#Funções lambdas - São pequenas funções anônimas. Ex:>>
+
+potenciacao =lambda x,y: x**y
+
+print(potenciacao(10,2))# retornará 100, pois 10²=100
+
+#Tipos de comentários
+''' Comentário '''
+""" Comentário""" 
+#Usando algumas funções nativas do Python
+# capitalize() converte o primeiro caractere em Maiúscula
+nome = "cláudio"
+print(nome.capitalize())
+
+#find() e index() Retornam a posição da primeira ocorrência de um valor em uma string, caso não exista, find() retornará -1, já index() retornará mensagem de erro. Ex>>
+trecho = "Uma vez flamengo, sempre flamengo"
+print(trecho.find("Uma")) #retorna 0
+print(trecho.find("uma"))#retorna -1
+
+print(trecho.index("Uma")) #retorna 0
+"""print(trecho.index("uma")) da erro """ 
+
+#len() Retorna o tamanho da string Ex>>
+
+var="Vasco";
+print("A palavra", var, "tem",len(var), "letras");
+
+#split() Divide substrings encontradas a partir de um separador especificado e retorna uma lista. Ex>>
+print(trecho)
+
+print (trecho.split(","))
+
+#strip() Remove espaços das extremidades da string
+
+varTeste=" Hermanoteu na terra de Godah  "
+print(varTeste)
+var_sem_espaco= varTeste.strip()
+print(var_sem_espaco)
+
 
