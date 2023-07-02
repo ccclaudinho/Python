bd7354272fd7afe214c691bf9e743c90dfcbe9aa
Cláudio Roberto Celestino Júnior <112024522+ccclaudinho@users.noreply.github.com>
2023-07-02 05:32:01 -0300

    primeiro commit

---
+++ programa1.py
@@ -0,0 +1,37 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+# Operadores comuns fundamentais>> sem novivdades
+#// divisão com piso - arredonda pra baixo Ex:
+a=22;
+b=5;
+c=a//b;
+print(c)
+
+#para exponenciação, usar **
+
+#para módulo, usar %
+
+#Ordem de precedência >> PEMMDAS >> P-Parenteses, E-Exponenciação, Multiplicação, Módulo e Divisão ( na ordem que aparecer da esq. pra direita), A- Adição e S- Subtração#
+#Operadores de comparação >> sem novivdades, mas é importante observar que em Python, diferente é != Ex:
+var1=5
+var2=6
+teste = var1 != var2
+print (teste)
+
+#Operadores lógicos >> And, Or e Not - Sem novidades
+
+# Operadores de associação são usados para saber se um conjunto de operadores está presente em um objeto Ex: 
+
+Times_cariocas= "Flamengo","Fluminense", "Vasco", "Botafogo"
+
+verifica="São paulo" in Times_cariocas
+
+print(verifica) #falso
+
+verifica="Vasco" in Times_cariocas
+
+print (verifica) #verdadeiro
+
+#Funções em Python >> usamos def para definir uma função
+
